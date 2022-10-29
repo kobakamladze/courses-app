e643c72bf818d259427cfcc846e154d75484cee7
kobakamladze <shutakaia@gmail.com>
2022-10-29 14:20:34 +0400

    working on sourse editing feature

--- server/models/coursesModel.js
+++ server/models/coursesModel.js
@@ -1,17 +1,24 @@
-import fs from "fs";
 import { v4 as uuid } from "uuid";
 
-class Course {
-  constructor({ title, price, img }) {
-    this.title = title;
-    this.price = price;
-    this.img = img;
-    this.id = uuid();
-  }
+const coursesList = [
+  {
+    title: "Node JS",
+    price: "60$",
+    img: "https://ih1.redbubble.net/image.1637717834.1604/aps,504x498,small,transparent-pad,600x600,f8f8f8.u1.jpg",
+    id: 1,
+  },
+];
 
-  save() {
-    fs.write("../coursesList.txt");
-  }
+function course({ title, price, img }) {
+  const newCourse = { title, price, img, id: uuid() };
+
+  coursesList.push(newCourse);
+
+  const recentlyAddedCourse = coursesList.find(
+    (course) => course.id === newCourse.id
+  );
+
+  return recentlyAddedCourse;
 }
 
-export { Course };
+export { course, coursesList };
--- server/routes/coursesRouter.js
+++ server/routes/coursesRouter.js
@@ -1,9 +1,59 @@
 import express from "express";
 
+import { course, coursesList } from "../models/coursesModel.js";
+
 const coursesRouter = express.Router();
 
+function findCourseById(courseId) {
+  return coursesList.find((course) => course.id === courseId);
+}
+
 coursesRouter.get("/", (req, res) =>
-  res.render("courses", { title: "All Courses" })
+  res.render("courses", { title: "All Courses", coursesList })
 );
+
+coursesRouter.get("/:courseId", (req, res) => {
+  const courseId = +req.params.courseId;
+  const courseFoundById = findCourseById(courseId);
+
+  console.log(JSON.stringify(req.params));
+  console.log(JSON.stringify(courseFoundById));
+
+  const { img, title, price } = courseFoundById;
+
+  res.render("course", { img, title, price });
+});
+
+coursesRouter.get("/:courseId/edit", (req, res) => {
+  if (!req.query.allow) {
+    return res.redirect("/");
+  }
+
+  const courseId = +req.params.courseId;
+  const courseFoundById = findCourseById(courseId);
+  const { id, img, title } = courseFoundById;
+
+  console.log("ID === " + id);
+
+  return res.render("courseEdit", { id, img, title });
+});
+
+coursesRouter.put("/:courseId/edit", (req, res) => {
+  const courseId = +req.params.courseId;
+  const courseFoundById = findCourseById(courseId);
+
+  console.log(JSON.stringify(courseId));
+  console.log(JSON.stringify(courseFoundById));
+  console.log("PARAMS === " + JSON.stringify(req.params));
+
+  const { price: oldPrice, img: oldImg, titel: oldTitle } = courseFoundById;
+  const { price: newPrice, img: newImg, titel: newTitle } = req.body;
+
+  courseFoundById.price = newPrice ? newPrice : oldPrice;
+  courseFoundById.title = newTitle ? newTitle : oldTitle;
+  courseFoundById.img = newImg ? newImg : oldImg;
+
+  res.render("courses");
+});
 
 export { coursesRouter };
